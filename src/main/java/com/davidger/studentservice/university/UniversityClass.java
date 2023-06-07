c6b0f721f93bce3267f6e398b9a9d0a1afa2faec
Ger <gerdavid7@gmail.com>
2023-06-07 19:53:53 +0300

    test commit2

--- src/main/java/com/davidger/studentservice/university/UniversityClass.java
+++ src/main/java/com/davidger/studentservice/university/UniversityClass.java
@@ -1,7 +1,12 @@
 package com.davidger.studentservice.university;
 
+import com.davidger.studentservice.student.Student;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Setter @Getter
 @AllArgsConstructor @NoArgsConstructor
@@ -11,4 +16,20 @@ public class UniversityClass {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer courseId;
+
+    @Column(nullable = false)
+    private String professor;
+
+    @Column(nullable = false)
+    private String description;
+
+    @JsonManagedReference
+    @ManyToMany(mappedBy = "universityClasses", fetch = FetchType.EAGER)
+    private Set<Student> student= new HashSet<>();
 }
